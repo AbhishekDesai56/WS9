5318984d48bdee272bbc738137f57333966d7615
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-02 08:32:49 +0530

    delete list is not working it is fix

--- js/test_home.js
+++ js/test_home.js
@@ -45,7 +45,7 @@ const getDeptHtml = (deptList) => {
 }
 
 const remove = (node) => {
-  let empPayrollData = empPayrollList.find(empData => empData._id == node._id)
+  let empPayrollData = empPayrollList.find(empData => empData._id == node.id)
   if(!empPayrollData) return;
   const index = empPayrollList
                 .map(empData => empData._id)
